b9d417eebb21a5928e9872600074ccb9e5d70f0f
FesalBadday <fesal.n04@hotmail.com>
2021-11-29 13:31:07 -0700

    tweaks

--- js/client.js
+++ js/client.js
@@ -1,1 +1,16 @@
-console.log('fdfdfdfdf')
+const init = async () => {
+  const response = await fetch('/.netlify/functions/post')
+  const data = await response.json();
+
+  const post = data.find((item) => item.caption.startsWith('#netlify'));
+
+  document.querySelector('.container').innerHTML = `
+    <figure>
+      <img src="${post.url}" alt="test">
+      <figcaption>${post.caption}</figcaption>
+    </figure>
+  `
+  console.log(data);
+}
+
+init();
